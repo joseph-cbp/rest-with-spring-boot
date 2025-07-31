65435358eec2d525a318b9642568524826f06688
Joseph Bezerra <joseph.carlosx@gmail.com>
2025-07-31 15:45:26 -0400

    first commit

---
+++ src/main/java/br/com/joseph/RestWithSpringBootApplication.java
@@ -0,0 +1,14 @@
+package br.com.joseph;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/joseph/models/Greeting.java
@@ -0,0 +1,5 @@
+package br.com.joseph.models;
+
+public record Greeting(long id, String content) {
+
+}
