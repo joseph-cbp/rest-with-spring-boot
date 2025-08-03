aa98b15a2fc876965899902ebce7b1a8eafecc82
Joseph Bezerra <joseph.carlosx@gmail.com>
2025-08-03 17:34:56 -0400

    feat: add subtraction path

--- src/main/java/br/com/joseph/controllers/MathOperation.java
+++ src/main/java/br/com/joseph/controllers/MathOperation.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.joseph.exceptions.UnsupportedMathOperationException;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/math")
@@ -22,6 +25,20 @@ public class MathOperation {
             Double[] arrDoubles = {number1, number2, (number1 + number2)};
             return arrDoubles;
     }
+
+    @RequestMapping("/subtract/{numberOne}/{numberTwo}")
+    public Double subtract(
+        @PathVariable("numberOne") String numberOne,
+        @PathVariable("numberTwo") String numberTwo
+        
+    ) throws Exception {
+        if (!isNumber(numberOne) || !isNumber(numberTwo)) throw new UnsupportedMathOperationException("Is not a Number");
+
+        Double firstNumber = convertToDouble(numberOne);
+        Double secondNumber = convertToDouble(numberTwo);
+
+        return firstNumber - secondNumber;
+    };
 
     public Boolean isNumber(String number) throws Exception {
         if (number == null || number.isEmpty()) throw new UnsupportedMathOperationException("Please set a numeric Value");
