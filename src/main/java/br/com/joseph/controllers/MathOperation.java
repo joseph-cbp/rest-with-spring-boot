cd899b80c1d31b397b27931d5f27e327889cd378
Joseph Bezerra <joseph.carlosx@gmail.com>
2025-08-06 19:59:12 -0400

    feat: create subtraction method

--- src/main/java/br/com/joseph/controllers/MathOperation.java
+++ src/main/java/br/com/joseph/controllers/MathOperation.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.joseph.exceptions.UnsupportedMathOperationException;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import br.com.joseph.services.ValidationHandler;
+
 
 
 @RestController
@@ -18,11 +18,11 @@ public class MathOperation {
         @PathVariable("numberOne") String numberOne, 
         @PathVariable("numberTwo") String numberTwo
         ) throws Exception {
-            if (!isNumber(numberOne) || !isNumber(numberTwo)) throw new UnsupportedMathOperationException("is not a Number");
+            if (!ValidationHandler.isNumber(numberOne) || !ValidationHandler.isNumber(numberTwo)) throw new UnsupportedMathOperationException("is not a Number");
 
-            Double number1 = convertToDouble(numberOne);
-            Double number2 = convertToDouble(numberTwo);
-            Double[] arrDoubles = {number1, number2, (number1 + number2)};
+            Double firstNumber = ValidationHandler.convertToDouble(numberOne);
+            Double secondNumber = ValidationHandler.convertToDouble(numberOne);
+            Double[] arrDoubles = {firstNumber, secondNumber, (firstNumber + secondNumber)};
             return arrDoubles;
     }
 
@@ -32,23 +32,11 @@ public class MathOperation {
         @PathVariable("numberTwo") String numberTwo
         
     ) throws Exception {
-        if (!isNumber(numberOne) || !isNumber(numberTwo)) throw new UnsupportedMathOperationException("Is not a Number");
+        if (!ValidationHandler.isNumber(numberOne) || !ValidationHandler.isNumber(numberTwo)) throw new UnsupportedMathOperationException("Is not a Number");
 
-        Double firstNumber = convertToDouble(numberOne);
-        Double secondNumber = convertToDouble(numberTwo);
+        Double firstNumber = ValidationHandler.convertToDouble(numberOne);
+        Double secondNumber = ValidationHandler.convertToDouble(numberTwo);
 
         return firstNumber - secondNumber;
     };
-
-    public Boolean isNumber(String number) throws Exception {
-        if (number == null || number.isEmpty()) throw new UnsupportedMathOperationException("Please set a numeric Value");
-
-        String srtNumber = number.replace(",", ".");
-
-        return srtNumber.matches("[-+]?[0-9]*\\.?[0-9]+");
-    }
-
-    public Double convertToDouble(String number) {
-        return Double.parseDouble(number);
-    }
 }
